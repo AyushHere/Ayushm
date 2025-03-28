34f91660349bc70ecdc5c4380db83eb6750b9e52
AyushHere <aayushmalviya90@gmail.com>
2025-03-29 03:22:59 +0530

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponentComponent } from './layout-component/layout-component.component';
 
 const routes: Routes = [];
 
